36f7f6529fbe80dd01fdd1dff4033cb750d2fff0
KeshaPatel7 <keshapatel@github.com>
2023-03-13 00:04:30 -0400

    made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner name = new Scanner(System.in);
+        System.out.println("Hey! What's your name?");
+        String fName = name.nextLine();
+
+        if( (fName.equals("Alice")) || (fName.equals("Bob")) ){
+            System.out.println("Hi" + fName + "How are you");
+        }
+        else{
+            System.out.println("who are you?");
+
+        }
 
     }
 }
